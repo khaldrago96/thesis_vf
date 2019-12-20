637932468e1b6c0b21d923acbcb0d5be73113da0
Delvin Widjaja <delvin.widjaja@gmail.com>
2019-12-20 01:50:24 +0100

    add new piano using buzz.js w/ salmon background

--- src/violin/violin.js
+++ src/violin/violin.js
@@ -1,11 +1,35 @@
+/* Testing BuzzJS */
+var mySound = new buzz.sound("../../resources/audio file/piano/040-c.wav");
+let audioFiles = [
+  "../../resources/audio file/piano/040-c.wav",
+  "../../resources/audio file/piano/042-d.wav",
+  "../../resources/audio file/piano/044-e.wav",
+  "../../resources/audio file/piano/045-f.wav",
+  "../../resources/audio file/piano/047-g.wav",
+  "../../resources/audio file/piano/049-a.wav",
+  "../../resources/audio file/piano/051-b.wav",
+  "../../resources/audio file/piano/052-c2.wav",
+  "../../resources/audio file/piano/041-cx.wav",
+  "../../resources/audio file/piano/043-dx.wav",
+  "../../resources/audio file/piano/046-fx.wav",
+  "../../resources/audio file/piano/048-gx.wav",
+  "../../resources/audio file/piano/050-ax.wav"
+];
+let pianoBuzz = document.getElementById("piano");
+
+pianoBuzz.addEventListener("mousedown", e => {
+  let playSound = new buzz.sound(audioFiles[e.target.dataset.note]);
+  playSound.play();
+});
+
 /*
-  Buttons for rhytm training
+  Piano using ToneJS
 */
 const synth = new Tone.Synth();
 // synth.oscillator.type = "sine";
 synth.toMaster();
 
-const piano = document.getElementById("piano");
+const piano = document.getElementById("piano2");
 
 piano.addEventListener("mousedown", e => {
   synth.triggerAttackRelease(e.target.dataset.note);
@@ -186,66 +210,3 @@ function reset() {
   collections = [];
   pianoRecord = [];
 }
-
-// const synth = new Tone.Synth();
-// synth.oscillator.type = "sine";
-// synth.toMaster();
-
-// const piano = document.getElementById("piano");
-
-// piano.addEventListener("mousedown", e => {
-//   synth.triggerAttack(e.target.dataset.note);
-// });
-
-// piano.addEventListener("mouseup", e => {
-//   synth.triggerRelease();
-// });
-
-// document.addEventListener("keydown", e => {
-//   switch (e.key) {
-//     case "d":
-//       return synth.triggerAttack("C4");
-//     case "r":
-//       return synth.triggerAttack("C#4");
-//     case "f":
-//       return synth.triggerAttack("D4");
-//     case "t":
-//       return synth.triggerAttack("D#4");
-//     case "g":
-//       return synth.triggerAttack("E4");
-//     case "h":
-//       return synth.triggerAttack("F4");
-//     case "u":
-//       return synth.triggerAttack("F#4");
-//     case "j":
-//       return synth.triggerAttack("G4");
-//     case "i":
-//       return synth.triggerAttack("G#4");
-//     case "k":
-//       return synth.triggerAttack("A4");
-//     case "o":
-//       return synth.triggerAttack("A#4");
-//     case "l":
-//       return synth.triggerAttack("B4");
-//     default:
-//       return;
-//   }
-// });
-
-// document.addEventListener("keyup", e => {
-//   switch (e.key) {
-//     case "d":
-//     case "r":
-//     case "f":
-//     case "t":
-//     case "g":
-//     case "h":
-//     case "u":
-//     case "j":
-//     case "i":
-//     case "k":
-//     case "o":
-//     case "l":
-//        synth.triggerRelease();
-//   }
-// });
